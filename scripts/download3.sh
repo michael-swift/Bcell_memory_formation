f81a6af223075c819b2ebd16d5c879b772dc12c8
Michael <swiftmichael26@gmail.com>
2022-10-19 17:03:06 -0700

    clean up

--- scripts/download3.sh
+++
@@ -1,4 +0,0 @@
-# Downloads the data from sherlock to local using rsync
-# excludes file patterns I don't want
-rsync -avP --exclude "*.bam*" --exclude "_*" --exclude "*SC_MULTI*" mswift2@login.sherlock.stanford.edu:/oak/stanford/groups/quake/mswift/TBD6_gex_ab/* .
-
