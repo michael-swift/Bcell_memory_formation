d18016e200f1aaa334896122977aef2a5b05cc56
Michael <swiftmichael26@gmail.com>
2024-05-25 11:02:40 -0700

    committing local changes (no major changes) before merging w remote. want to commit before doing major work on revision e.g. batch effects questions

--- snakemake_workflow/scripts/gex_preprocess/train_scvi_all.py
+++ snakemake_workflow/scripts/gex_preprocess/train_scvi_all.py
@@ -1,67 +1,81 @@
 #!/usr/bin/env python
+
+import argparse
 import numpy as np
 import pandas as pd
-import scvi
-import argparse
 import scanpy as sc
+import scvi
 
-# read h5ad
-parser = argparse.ArgumentParser(description="run scvi")
-parser.add_argument("input_file", help="h5ad anndata")
-parser.add_argument("-output_file", help="name of output file")
-parser.add_argument(
-    "-output_model",
-    default=".",
-    help="model output directory (default: working directory)",
-)
-parser.add_argument(
-    "-covariate_genes",
-    help="tabular data of genes associated with cell cycle, sex, and cell stress",
-)
-parser.add_argument(
-    "-subsample",
-    default="False",
-    help="whether or not to subsample the input data (for speed of testing different model constructions)",
-)
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Run scVI")
+    parser.add_argument("input_file", help="h5ad anndata")
+    parser.add_argument("-output_file", help="Name of output file")
+    parser.add_argument(
+        "-output_model",
+        default=".",
+        help="Model output directory (default: working directory)"
+    )
+    parser.add_argument(
+        "-covariate_genes",
+        help="Tabular data of genes associated with cell cycle, sex, and cell stress"
+    )
+    parser.add_argument(
+        "-subsample",
+        default="False",
+        help="Whether or not to subsample the input data (for speed of testing different model constructions)"
+    )
 
-args = parser.parse_args()
-FILENAME = args.input_file
-outfile = args.output_file
-out_dir = args.output_model
-covariate_genes = args.covariate_genes
-subsample = args.subsample
-print(type(subsample))
-print("loading data")
-adata = sc.read_h5ad(FILENAME)
-layer = "counts"
-if subsample == "True":
-    sc.pp.subsample(adata, n_obs=100000)
+    args = parser.parse_args()
+    filename = args.input_file
+    outfile = args.output_file
+    out_dir = args.output_model
+    covariate_genes = args.covariate_genes
+    subsample = args.subsample
 
-# setup batch column
-adata.obs["donor_tissue"] = (
-    adata.obs["donor"].astype(str) + "_" + adata.obs["tissue"].astype(str)
-)
+    print(type(subsample))
+    print("Loading data")
+    adata = sc.read_h5ad(filename)
+    layer = "counts"
 
-# setup vanilla model:
-scvi.model.SCVI.setup_anndata(adata, layer=layer, batch_key="donor_tissue")
-vae = scvi.model.SCVI(adata, n_layers=2, n_latent=30, gene_likelihood="nb")
-# train
-vae.train(max_epochs=150)
-# get latent rep
-Z_hat = vae.get_latent_representation()
-# calculate umap using latent representation
-adata.obsm["X_scVI_all"] = Z_hat
+    if subsample == "True":
+        sc.pp.subsample(adata, n_obs=100000)
 
-add_cellbender_model = False
-if add_cellbender_model:
-    # setup vanilla model:
-    layer = "cellbender_counts"
+    # Setup batch column
+    adata.obs["donor_tissue"] = (
+        adata.obs["donor"].astype(str) + "_" + adata.obs["tissue"].astype(str)
+    )
+
+    # Setup vanilla model
     scvi.model.SCVI.setup_anndata(adata, layer=layer, batch_key="donor_tissue")
     vae = scvi.model.SCVI(adata, n_layers=2, n_latent=30, gene_likelihood="nb")
-    # train
+
+    # Train
     vae.train(max_epochs=150)
-    # get latent rep
-    Z_hat = vae.get_latent_representation()
-    # calculate umap using latent representation
-    adata.obsm["X_scVI_cellbender_all"] = Z_hat
-adata.write_h5ad(outfile)
+
+    # Get latent representation
+    z_hat = vae.get_latent_representation()
+
+    # Calculate UMAP using latent representation
+    adata.obsm["X_scVI_all"] = z_hat
+
+    add_cellbender_model = False
+    if add_cellbender_model:
+        # Setup vanilla model
+        layer = "cellbender_counts"
+        scvi.model.SCVI.setup_anndata(adata, layer=layer, batch_key="donor_tissue")
+        vae = scvi.model.SCVI(adata, n_layers=2, n_latent=30, gene_likelihood="nb")
+
+        # Train
+        vae.train(max_epochs=150)
+
+        # Get latent representation
+        z_hat = vae.get_latent_representation()
+
+        # Calculate UMAP using latent representation
+        adata.obsm["X_scVI_cellbender_all"] = z_hat
+
+    adata.write_h5ad(outfile)
+
+if __name__ == "__main__":
+    main()
